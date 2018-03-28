decbaeac97a6a77b2aaad110c0eae6896a9c443c
Charlie Lin <charlielin@Charlies-MacBook-Pro.local>
2018-03-28 11:44:01 -0400

    change base64 conversion to use scipy

--- server/utils/convert.py
+++ server/utils/convert.py
@@ -4,13 +4,10 @@ import base64
 import cv2
 import numpy as np
 from PIL import Image
-
-def toRGB(image):
-    return cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
+import scipy
 
 def base64_to_array(encoded):
-    imgdata = base64.b64decode(encoded)
-    return toRGB(Image.open(io.BytesIO(imgdata)))
+    return scipy.misc.imread(io.BytesIO(base64.b64decode(encoded)), mode='RGB')
 
 def array_to_base64(array):
     retval, buffer = cv2.imencode('.png', array)
